25236c427e0a8eb7360bd1049bf1c756ee1963fa
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-09-17 14:15:48 +0900

    回転するためにrotateValueを定数宣言する

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 var circle = document.getElementById("circle");
 var upBtn = document.getElementById("upBtn");
 var downBtn = document.getElementById("downBtn");
+
+var rotateValue = circle.style.transform;
