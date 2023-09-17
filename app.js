49f836ec7add1f7da2450fbb15f1eb33922189bb
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-09-17 14:30:13 +0900

    90度回転する機能を作成

--- app.js
+++ app.js
@@ -5,3 +5,9 @@ var downBtn = document.getElementById("downBtn");
 var rotateValue = circle.style.transform;
 //console.log(rotateValue);
 var rotateSum;
+
+upBtn.onclick = function() {
+    /*時計回りに90度回転させる*/ 
+    rotateSum = rotateValue + "rotate(-90deg)";
+    circle.style.transform = rotateSum;
+};
